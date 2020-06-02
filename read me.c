086dd6e0ed98d8fd4a4c32c714cab9ca791823fb
aliShallal <66331116+aliShallal@users.noreply.github.com>
2020-06-02 12:36:58 -0700

    Update read me.c

--- read me.c
+++ read me.c
@@ -1,5 +1,5 @@
 compiler: 
-gcc Homework2.c -o <nom du fichier de sortie>
+gcc Homework2.c -o filename
 courir:
 ./ <output-filename> n 
-exemple: ./Homework2 1000 // ici n = 1000, une valeur plus élevée de 'n' donne des résultats plus précis.
+exemple: ./Homework2 1000 
