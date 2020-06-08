f17bb85778e0b80fb7aeda9c2f7b648135f97065
aliShallal <66331116+aliShallal@users.noreply.github.com>
2020-06-08 09:37:31 -0700

    Update task2 a.c

--- task2 a.c
+++ task2 a.c
@@ -1,7 +1,7 @@
-# inclut < stdio.h > 
-# inclut < stdlib.h > 
-# inclut < sys / wait.h > 
-# inclut < sys / types.h >
+# include < stdio.h > 
+# include < stdlib.h > 
+# include < sys / wait.h > 
+# include < sys / types.h >
 # include < unistd.h > 
 
   
